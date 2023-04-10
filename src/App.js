acdeddfda2da18ea8e23010694d18735d1e44457
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-04-10 22:21:48 +0300

    itemler ve search yapıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
+import { useState } from 'react';
 import './App.css';
 import Index from './components/Index';
+import { SimpleGrid, Input } from '@mantine/core';
+
+
+const basketItems = [
+  {
+    id: 1,
+    name: 'Headset',
+    src: 'headset',
+  },
+  {
+    id: 2,
+    name: 'Keyboard',
+    src: 'keyboard',
+  },
+  {
+    id: 3,
+    name: 'Laptop',
+    src: 'laptop',
+  },
+  {
+    id: 4,
+    name: 'Mouse',
+    src: 'mouse',
+  },
+  {
+    id: 5,
+    name: 'Phone',
+    src: 'phone',
+  },
+  {
+    id: 6,
+    name: 'Watch',
+    src: 'watch',
+  },
+];
 
 function App() {
+
+  const [searchValue, setSearchValue] = useState('');
+  const filtered = basketItems.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0)
+
   return (
     <div className="App">
-      <Index />
+      <Input.Wrapper label="Search" className='search-bar'>
+        <Input onChange={(e) => setSearchValue(e.target.value)} />
+      </Input.Wrapper>
+
+      <SimpleGrid
+        cols={3}
+        spacing="lg">
+        {filtered.map(({ name, id, src }) => (
+          <div key={id} ><Index name={name} src={src} /></div>
+        ))}
+      </SimpleGrid>
     </div>
   );
 }
--- src/components/Index.js
+++ src/components/Index.js
@@ -1,18 +1,18 @@
 import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
 
-function Index() {
+function Index({ name, src }) {
     return (
         <Card shadow="sm" padding="lg" radius="md" withBorder>
             <Card.Section>
                 <Image
-                    src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                    src={`/assets/images/${src}.jpg`}
                     height={160}
                     alt="Norway"
                 />
             </Card.Section>
 
             <Group position="apart" mt="md" mb="xs">
-                <Text weight={500}>Norway Fjord Adventures</Text>
+                <Text weight={500}>{name}</Text>
                 <Badge color="pink" variant="light">
                     On Sale
                 </Badge>
@@ -30,4 +30,4 @@ function Index() {
     )
 }
 
-export default Index
+export default Index;
